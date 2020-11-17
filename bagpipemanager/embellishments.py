94cb2bf634548e05bbf8e33c140cd9bae849f256
Victor Torres <victorhxtorres@gmail.com>
2020-11-16 22:47:34 -0500

    embellishments as regex, bpm exception

--- bagpipemanager/embellishments.py
+++ bagpipemanager/embellishments.py
@@ -1,15 +1,15 @@
 class Embellishments:
-    GRACE_NOTES = "[1]g"
-    DOUBLINGS = "[0,1]db[1,2]"
-    GRACE_NOTES_STRIKES_STRIKES = "[0,2]st[1,2]"
-    PELES = "[0,2]pel[1,2]"
-    BIRLS = "[0,1]br[0,1]"
-    D_THROWS = "{0,2}thrd"
-    GRIPS = "[0,1]grp[1,2]"
-    TAORLUATHS = "tar{0,1}"
-    BUBBLYS = "{0,1}bubly"
-    ACCIDENTALS_NATURAL = "natural{1,2}"
-    ACCIDENTALS_SHARP = "sharp{1,2}"
+    GRACE_NOTES = r"[a-z]{1}g"
+    DOUBLINGS = r"[a-z]{0,1}db[a-z]{1,2}"
+    GRACE_NOTES_STRIKES_STRIKES = r"[a-z]{0,2}st[a-z]{1,3}"
+    PELES = r"[a-z]{0,2}pel[a-z]{1,2}"
+    BIRLS = r"[a-z]{0,1}br[a-z]{0,1}"
+    D_THROWS = r"[a-z]{0,2}thrd"
+    GRIPS = r"[a-z]{0,1}grp[a-z]{1,2}"
+    TAORLUATHS = r"tar[a-z]{0,1}"
+    BUBBLYS = r"[a-z]{0,1}bubly"
+    ACCIDENTALS_NATURAL = r"natural[a-z]{1,2}"
+    ACCIDENTALS_SHARP = r"sharp[a-z]{1,2}"
 
     def __init__(self):
         pass
---
+++ bagpipemanager/exceptions.py
@@ -0,0 +1,2 @@
+class BagpipeManagerException(BaseException):
+    pass
