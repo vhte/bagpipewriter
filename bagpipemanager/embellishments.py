8565f7fc3288ad00ec30a908b943d0be9fa4415a
Victor Torres <talk@victortorr.es>
2020-11-22 00:09:53 -0500

    completed toggle embellishments

--- bagpipemanager/embellishments.py
+++ bagpipemanager/embellishments.py
@@ -1,15 +1,15 @@
 class Embellishments:
-    GRACE_NOTES = r"[a-z]{1}g"
-    DOUBLINGS = r"[a-z]{0,1}db[a-z]{1,2}"
-    GRACE_NOTES_STRIKES_STRIKES = r"[a-z]{0,2}st[a-z]{1,3}"
-    PELES = r"[a-z]{0,2}pel[a-z]{1,2}"
-    BIRLS = r"[a-z]{0,1}br[a-z]{0,1}"
-    D_THROWS = r"[a-z]{0,2}thrd"
-    GRIPS = r"[a-z]{0,1}grp[a-z]{1,2}"
-    TAORLUATHS = r"tar[a-z]{0,1}"
-    BUBBLYS = r"[a-z]{0,1}bubly"
-    ACCIDENTALS_NATURAL = r"natural[a-z]{1,2}"
-    ACCIDENTALS_SHARP = r"sharp[a-z]{1,2}"
+    GRACE_NOTES = r"([a-g,t]{1}g)"
+    DOUBLINGS = r"([a-z]{0,1}db[a-z]{1,2})"
+    GRACE_NOTES_STRIKES_STRIKES = r"([a-z]{0,2}st[a-z]{1,3})"
+    PELES = r"([a-z]{0,2}pel[a-z]{1,2})"
+    BIRLS = r"([a-z]{0,1}br[a-z]{0,1})"
+    D_THROWS = r"([a-z]{0,2}thrd)"
+    GRIPS = r"([a-z]{0,1}grp[a-z]{0,2})"
+    TAORLUATHS = r"(tar[a-z]{0,1})"
+    BUBBLYS = r"([a-z]{0,1}bubly)"
+    ACCIDENTALS_NATURAL = r"(natural[a-z]{1,2})"
+    ACCIDENTALS_SHARP = r"(sharp[a-z]{1,2})"
 
     def __init__(self):
         pass
--- bagpipemanager/sheet.py
+++ bagpipemanager/sheet.py
@@ -7,30 +7,22 @@ class Sheet:
     TUNE_TEMPO = "TuneTempo"
     STAFF = r"& sharpf sharpc 4_4"  # TODO space regex + use group to replace in file?
 
-    def __init__(self, score, tempo=0):
-        self._header, self._tune = self._separate_header_tune(score)
+    def __init__(self, file_content, tempo=0):
+        self._header, self._score = self._separate_header_tune(file_content)
         self._tempo = tempo
         self._embellishments = Embellishments()
 
     @property
     def score(self):
-        return self._header + "\n" + self.STAFF + self._score
+        return self._header + self.STAFF + self._score
 
     @score.setter
     def score(self, content):
-        self._header, self._tune = self._separate_header_tune(content)
-
-    @property
-    def tune(self):
-        return self._score
-
-    @tune.setter
-    def tune(self, content):
-        self._score = content
+        self._header, self._score = self._separate_header_tune(content)
 
     @property
     def tempo(self):
-        tempo = re.findall(r"TuneTempo[\s]*,[\s]*(?P<tempo>[\d]+)", self._score)
+        tempo = re.findall(r"TuneTempo[\s]*,[\s]*(?P<tempo>[\d]+)", self._header)
         if tempo:
             return int(tempo[0])
         raise ValueError("Couldn't find {} in loaded score.".format(self.TUNE_TEMPO))
@@ -38,20 +30,20 @@ class Sheet:
     @tempo.setter
     def tempo(self, value):
         pattern = re.compile(r"TuneTempo[\s]*,[\s]*[\d]+")
-        self._score = pattern.sub("TuneTempo,{}".format(value), self._score)
+        self._header = pattern.sub("TuneTempo,{}".format(value), self._header)
 
     def toggle_embellishments(self, disable):
         embellishments = self._embellishments.get_all()
         for embellishment in embellishments:
             if disable is True:
-                pattern = re.compile(embellishment)
-                self._score = pattern.sub("\"{}\"".format(embellishment), self._score)
+                self._score = re.sub(embellishment, r'"\1"', self._score)
             else:
-                pattern = re.compile("\"{}\"".format(embellishment))
-                self._score = pattern.sub(embellishment, self._score)
+                self._score = re.sub(
+                    '"{}"'.format(embellishment), r"\1".replace('"', ""), self._score
+                )
 
     def toggle_repetition(self, disable):
-        # TODO improve sub() use
+        # TODO improve sub() use like toggle_embellishments()
         if disable is True:
             pattern = re.compile(r"(I!'')")
             self._score = pattern.sub("I!\"'''\"", self._score)
@@ -70,8 +62,12 @@ class Sheet:
     def _separate_header_tune(self, content):
         """Divides the sheet content into header and tune"""
         try:
-            self._header, self._score = content.split(self.STAFF)
+            header, score = content.split(self.STAFF)
         except ValueError as ve:
-            raise BagpipeManagerException(r"None or more than one staff signature ({}) found in .bww file: {}".format(self.STAFF, str(ve)))
+            raise BagpipeManagerException(
+                r"None or more than one staff signature ({}) found in .bww file: {}".format(
+                    self.STAFF, str(ve)
+                )
+            )
 
-        return self._header, self._score
+        return header, score
